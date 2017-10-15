6ff403c779187d851564f3aa9f33bf24a5e27dd5
Folk <vhaarr@gmail.com>
2017-10-15 17:36:51 +0200

    Initial upload.

---
+++ modinfo.lua
@@ -0,0 +1,15 @@
+-- modinfo read by stlrel from http://github.com/stellaris-mods/scripts
+return {
+	path = "Spaceport",
+	name = "Spaceport (1.8)",
+	tags = { "Gameplay", "Spaceports" },
+	picture = "thumb.jpg",
+	supported_version = "1.8.*",
+	remote_file_id = 965209251,
+	--readme = "README.md",
+	--steambb = "steam.bbcode",
+	--bbheadinglevel = 3,
+	-- Files and folders added in exclude will not be copied over to the Paradox Interactive
+	-- local mod folder when you run `stlrel --steam`.
+	exclude = { ".git" }
+}
